8af5d53233ae94ba29b51ac939cf402bb51f8820
Viacheslav Kharchenko <v.kharchenko@mobidev.biz>
2022-06-25 21:24:53 +0300

    Index page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
   def index
   end
+
+  def delete_all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root "articles#index"
+  get "/delete_all", to: "articles#delete_all", as: "delete_all"
 end
